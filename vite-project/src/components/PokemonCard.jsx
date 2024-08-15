cfb35bcbd671ed78ae87bc692e1cbd2bed63210f
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-08-14 21:54:44 -0400

    DONE

--- vite-project/src/components/PokemonCard.jsx
+++ vite-project/src/components/PokemonCard.jsx
@@ -1,19 +1,32 @@
 // TODO: This component should render a single pokemon's stats and image.
+import { useState } from "react";
 
-const PokemonCard = () => {
+const PokemonCard = ({frontImage, backImage, hp, name}) => {
+    const [isFront, setIsFront] = useState(frontImage);
+
+    const toggleFlip = () => {
+        if(isFront === frontImage){
+            setIsFront(backImage)
+        }else{
+            setIsFront(frontImage)
+        }     
+    };
+
+    // console.log(isFront)
+
     return (
-        <div className="ui card">
+        <div className="ui card" onClick={toggleFlip}>
             <div>
-                <div className="image">
-                    <img alt="pokemon name" src="" />
+                <div className="image" >
+                    <img alt="pokemon name" src={isFront} />
                 </div>
                 <div className="content">
-                    <div className="header">Pokemon name</div>
+                    <div className="header">{name}</div>
                 </div>
                 <div className="extra content">
                     <span>
                         <i className="icon heartbeat red" />
-                        Pokemon HP
+                        {hp}
                     </span>
                 </div>
             </div>
--- vite-project/src/context/PokemonContext.jsx
+++ vite-project/src/context/PokemonContext.jsx
@@ -1,1 +1,10 @@
 // TODO: Create the context
+
+
+// PokemonContext.jsx
+import { createContext } from "react"; // importing context from react
+
+const PokemonContext = createContext(); // creating context 
+
+
+export default PokemonContext; // exporting it 
